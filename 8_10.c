a765bedfc80d21a06d958c14f22a60ef21576e41
liutianqiJoey <88476491+liutianqiJoey@users.noreply.github.com>
2021-08-11 11:15:20 +0800

    Add files via upload

---
+++ 8_10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int ch = getchar();
+	printf("%d\n", ch);
+	putchar(ch);
+	/*int n = 1;
+	while(n <= 10)
+	{
+		printf("%d\t", n);
+		n++;
+	}
+
+	for (int m=1; m <= 10; m ++ )
+	{
+		printf(" % d\t", m);
+	}*/
+	return 0;
+}
